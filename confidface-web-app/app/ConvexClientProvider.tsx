56a30d29bd546c3e243a49810e437ffee641f370
bcpethum <bcpethum@gmail.com>
2025-10-21 02:43:16 +0530

    setup database to get data from user informations

--- confidface-web-app/app/ConvexClientProvider.tsx
+++ confidface-web-app/app/ConvexClientProvider.tsx
@@ -4,6 +4,7 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 import Provider from "./Provider";
 
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
--- confidface-web-app/convex/_generated/api.d.ts
+++ confidface-web-app/convex/_generated/api.d.ts
@@ -8,7 +8,7 @@
  * @module
  */
 
-import type * as schema from "../schema.js";
+import type * as users from "../users.js";
 
 import type {
   ApiFromModules,
@@ -25,7 +25,7 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  schema: typeof schema;
+  users: typeof users;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
 
