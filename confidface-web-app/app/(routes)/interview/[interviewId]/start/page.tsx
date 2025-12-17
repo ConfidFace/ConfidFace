fe43d9295676737179a8f60aa8d91cf79acdb377
AhmedHaseen <haseen2002hsn@gmail.com>
2025-12-17 11:29:40 +0530

    error resolve

--- confidface-web-app/app/(routes)/interview/[interviewId]/start/page.tsx
+++ confidface-web-app/app/(routes)/interview/[interviewId]/start/page.tsx
@@ -15,7 +15,6 @@ export type interviewData = {
   jobTitle: string | null;
   jobDescription: string | null;
   interviewQuestions: interviewQuestions[];
-  feedback?: any;
   userId: string | null;
   _id: string;
   resumeUrl: string | null;
