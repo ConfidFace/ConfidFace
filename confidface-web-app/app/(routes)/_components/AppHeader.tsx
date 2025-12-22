da504dbc86f7833973aa72da5cde144dee5525a3
AhmedHaseen <haseen2002hsn@gmail.com>
2025-12-22 15:59:31 +0530

    logo visibility in dashboard enabled

--- confidface-web-app/app/(routes)/_components/AppHeader.tsx
+++ confidface-web-app/app/(routes)/_components/AppHeader.tsx
@@ -25,7 +25,7 @@ function AppHeader() {
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
-        {/* <h1 className="text-base font-bold md:text-2xl">ConfidFace</h1> */}
+         <h1 className="text-base font-bold md:text-2xl">ConfidFace</h1>
       </div>
       <div>
         <ul className='flex gap-5'>
